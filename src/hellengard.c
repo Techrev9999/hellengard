efb3a282f4899611ec108ab273fbe63534e2cdba
Techrev <techrev@live.com>
2019-11-25 05:59:11 -0500

    modified:   src/hellengard.c

--- src/hellengard.c
+++ src/hellengard.c
@@ -28,17 +28,16 @@
 #include "vera.h"
 
 int main() {
-    //uint8_t p = 0;
-
+	//These functions are defined in vera.h.  This will not work without it, or without being linked to vera.lib
 	setDataPort(1, 0);  //set stride here and dataport
-	setScreenScale(64, 64);
-	mlayer0Setup(0x61, 0x00, L0_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);
-	mlayer1Setup(0x61, 0x01, L1_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);
+	setScreenScale(64, 64);  //set the scale.
+	mlayer0Setup(0x61, 0x00, L0_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);  //layer 0 configuration options
+	mlayer1Setup(0x61, 0x01, L1_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);  //layer 1 configuration options
 	copyData();   //Need to fix this function so data is on the C side and addresses are passed to veralib
-	// numMapCols, c, r, w, h, chr, clr, map address
-	fillWindow(32, 1, 1, 28, 28, 24, 0, L0_MAP_BASE);  // Still some bugs in here from the original.  I need to come up with a better system.  Works, though.
-	fillWindow(64, 1, 1, 28, 28, 0, 0, L1_MAP_BASE);
-	fillWindow(64, 0, 0, 40, 1, 3, 0, L1_MAP_BASE);
+	// number of columns, starting column, starting row, width, height, chacterr, color, layer map address
+	fillWindow(32, 1, 1, 28, 28, 24, 0, L0_MAP_BASE);  // Creates and fills an area with the selected char, using the selected font.
+	fillWindow(64, 1, 1, 28, 28, 0, 0, L1_MAP_BASE);   // Currently that font is defined and copied over in vera.lib, but I will move
+	fillWindow(64, 0, 0, 40, 1, 3, 0, L1_MAP_BASE);    // some of that to the C side soon.
 	fillWindow(64, 0, 29, 40, 1, 3, 0, L1_MAP_BASE);
 	fillWindow(64, 0, 0, 1, 30, 4, 0, L1_MAP_BASE);
 	fillWindow(64, 29, 0, 1, 30, 4, 0, L1_MAP_BASE);
@@ -46,7 +45,6 @@ int main() {
 	fillWindow(64, 30, 1, 9, 9, 1, 0, L1_MAP_BASE);
 	fillWindow(64, 29, 9, 11, 1, 3, 0, L1_MAP_BASE);
 	fillWindow(64, 30, 10, 9, 19, 2, 0, L1_MAP_BASE);
-	//printf("%u\n",p);
-	while(1){}
+	while(1){}   // An unending loop so you can bask in the glory of this demo forever.  And ever.  And ever.
     return 0;
  }
