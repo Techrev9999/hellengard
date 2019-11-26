c4c29fcb7426efc6bd25c6be400a5f6c1c52b53d
Techrev <techrev@live.com>
2019-11-26 08:56:09 -0500

    modified:   src/hellengard.c

--- src/hellengard.c
+++ src/hellengard.c
@@ -30,10 +30,11 @@
 int main() {
 	//These functions are defined in vera.h.  This will not work without it, or without being linked to vera.lib
 	setDataPort(1, 0);  //set stride here and dataport
-	setScreenScale(64, 64);  //set the scale.
+	setScreenScale(64, 64, 1);  //set the scale and set video mode to 1 (VGA)
 	mlayer0Setup(0x61, 0x00, L0_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);  //layer 0 configuration options
 	mlayer1Setup(0x61, 0x01, L1_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);  //layer 1 configuration options
 	copyData();   //Need to fix this function so data is on the C side and addresses are passed to veralib
+	
 	// number of columns, starting column, starting row, width, height, chacterr, color, layer map address
 	fillWindow(32, 1, 1, 28, 28, 24, 0, L0_MAP_BASE);  // Creates and fills an area with the selected char, using the selected font.
 	fillWindow(64, 1, 1, 28, 28, 0, 0, L1_MAP_BASE);   // Currently that font is defined and copied over in vera.lib, but I will move
@@ -44,7 +45,16 @@ int main() {
 	fillWindow(64, 39, 0, 1, 30, 4, 0, L1_MAP_BASE);
 	fillWindow(64, 30, 1, 9, 9, 1, 0, L1_MAP_BASE);
 	fillWindow(64, 29, 9, 11, 1, 3, 0, L1_MAP_BASE);
+	fillChar(64, 0, 0, 5, 0, L1_MAP_BASE);
+	fillChar(64, 29, 0, 9, 0, L1_MAP_BASE);
+	fillChar(64, 39, 0, 6, 0, L1_MAP_BASE);
+	fillChar(64, 0, 29, 7, 0, L1_MAP_BASE);
+	fillChar(64, 29, 29, 10, 0, L1_MAP_BASE);
+	fillChar(64, 39, 29, 8, 0, L1_MAP_BASE);
+	fillChar(64, 29, 9, 11, 0, L1_MAP_BASE);
+	fillChar(64, 39, 9, 12, 0, L1_MAP_BASE);
 	fillWindow(64, 30, 10, 9, 19, 2, 0, L1_MAP_BASE);
+
 	while(1){}   // An unending loop so you can bask in the glory of this demo forever.  And ever.  And ever.
     return 0;
  }
