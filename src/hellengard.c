a2eb4f72685f2482e7b48707ba09a1d6f0b20b14
Techrev <techrev@live.com>
2019-11-28 05:22:00 -0500

    modified:   src/font-hud.h 	modified:   src/hellengard.c 	new file:   src/res/font-hud.h 	modified:   src/res/font-hud.png

--- src/hellengard.c
+++ src/hellengard.c
@@ -9,12 +9,12 @@ int main() {
 	//These functions are defined in vera.h.  This will not work without it, or without being linked to vera.lib
 	setDataPort(1, 0);  //set stride here and dataport
 	setScreenScale(64, 64, 1);  //set the scale and set video mode to 1 (VGA)
-	mlayer0Setup(0x61, 0x00, L0_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);  //layer 0 configuration options
-	mlayer1Setup(0x61, 0x01, L1_MAP_BASE, FONT_LPETSCII, 0x0000, 0x0000);  //layer 1 configuration options
+	mlayer0Setup(0x61, 0x00, L0_MAP_BASE, FONT_UPETSCII, 0x0000, 0x0000);  //layer 0 configuration options
+	mlayer1Setup(0x61, 0x01, L1_MAP_BASE, FONT_UPETSCII, 0x0000, 0x0000);  //layer 1 configuration options
 	copyData(32, (uint16_t)(&palette), PALETTE);   //Need to fix this function so data is on the C side and addresses are passed to veralib
-	copyData(2048, (uint16_t)(&fonthud), FONT_LPETSCII) ;
+	copyData(4096, (uint16_t)(&fonthud), FONT_UPETSCII) ;
 	// number of columns, starting column, starting row, width, height, chacterr, color, layer map address
-	fillWindow(32, 1, 1, 28, 28, 63, 0, L0_MAP_BASE);  // Creates and fills an area with the selected char, using the selected font.
+	fillWindow(32, 1, 1, 28, 28, 64, 0, L0_MAP_BASE);  // Creates and fills an area with the selected char, using the selected font.
 	fillWindow(64, 1, 1, 28, 28, 0, 0, L1_MAP_BASE);   // Currently that font is defined and copied over in vera.lib, but I will move
 	fillWindow(64, 0, 0, 40, 1, 3, 0, L1_MAP_BASE);    // some of that to the C side soon.
 	fillWindow(64, 0, 29, 40, 1, 3, 0, L1_MAP_BASE);
